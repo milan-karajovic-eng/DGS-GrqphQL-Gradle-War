02dedf2ef7bbe258ffd70afb01120f403e0be67c
milan-karajovic-karajovic <milan.karajovic.eng@gmail.com>
2024-08-22 19:14:19 +0200

    Implementation for the Fetcher and Mutation regarding to the specification in the schema/schema.graphql. Fetcher and Mutation will use methods from Service to manipulate with data in database.

--- src/main/java/com/example/presscentric/test_milan_karajovic_presscetnric/fetcher/UserFetcher.java
+++ src/main/java/com/example/presscentric/test_milan_karajovic_presscetnric/fetcher/UserFetcher.java
@@ -2,7 +2,10 @@ package com.example.presscentric.test_milan_karajovic_presscetnric.fetcher;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.presscentric.test_milan_karajovic_presscetnric.domain.User;
+import com.example.presscentric.test_milan_karajovic_presscetnric.service.UserService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
 import com.netflix.graphql.dgs.InputArgument;
@@ -10,16 +13,18 @@ import com.netflix.graphql.dgs.InputArgument;
 @DgsComponent
 public class UserFetcher {
 	
+	@Autowired
+	UserService userService;
+	
+	
 	@DgsData(parentType = "Query", field = "users")
     public List<User> findAllUsers() {
-		// TODO: To be implemented
-		return null;
+        return userService.findAll();
     }
 	
 	@DgsData(parentType = "Query", field = "user")
     public User findUserById(@InputArgument("id") Integer id) {
-		// TODO: To be implemented
-		return null;
+		return userService.findUserById(id);
     }
 	
 }
--- src/main/java/com/example/presscentric/test_milan_karajovic_presscetnric/fetcher/UserMutation.java
+++ src/main/java/com/example/presscentric/test_milan_karajovic_presscetnric/fetcher/UserMutation.java
@@ -1,8 +1,11 @@
 package com.example.presscentric.test_milan_karajovic_presscetnric.fetcher;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.presscentric.test_milan_karajovic_presscetnric.domain.CreateUserInput;
 import com.example.presscentric.test_milan_karajovic_presscetnric.domain.UpdateUserInput;
 import com.example.presscentric.test_milan_karajovic_presscetnric.domain.User;
+import com.example.presscentric.test_milan_karajovic_presscetnric.service.UserService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
 import com.netflix.graphql.dgs.InputArgument;
@@ -12,22 +15,22 @@ import jakarta.validation.Valid;
 @DgsComponent
 public class UserMutation {
 	
+	@Autowired
+	UserService userService;
+	
 	@DgsData(parentType = "Mutation", field = "createUser")
     public User createUser(@InputArgument("input") CreateUserInput userInput) {
-		// TODO: To be implemented
-		return null;
+		return userService.save(new User(null, userInput.getName(), userInput.getEmail()));
 	}
 	
 	@DgsData(parentType = "Mutation", field = "updateUser")
     public User updateUser(@InputArgument("id") Integer id, @Valid @InputArgument("input") UpdateUserInput userInput) {
-		// TODO: To be implemented
-		return null;
+		return userService.updateUser(id, userInput.getName(), userInput.getEmail());
 	}
 	
 	@DgsData(parentType = "Mutation", field = "deleteUser")
     public User deleteUser(@InputArgument("id") Integer id) {
-		// TODO: To be implemented
-		return null;
+		return userService.deleteUserById(id);
 	}
 	
 }
